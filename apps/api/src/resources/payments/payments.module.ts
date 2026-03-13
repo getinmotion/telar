02a5a8457577b63d3c3c0c2998e88ff4e1e7e996
Cesar Narvaez <cesar.teacher24@gmail.com>
2026-03-12 23:45:38 -0500

    🧪Feat/Api - Se crea nueva migracion para el flujo de payouts

---
+++ apps/api/src/resources/payments/payments.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { PaymentsController } from './payments.controller';
+import { PaymentsService } from './payments.service';
+import { paymentsProviders } from './payments.providers';
+import { DatabaseModule } from 'src/config/configOrm.module';
+import { MailModule } from '../mail/mail.module';
+
+@Module({
+  imports: [DatabaseModule, MailModule],
+  controllers: [PaymentsController],
+  providers: [...paymentsProviders, PaymentsService],
+  exports: [PaymentsService],
+})
+export class PaymentsModule {}
